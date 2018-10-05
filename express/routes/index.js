f610930e732100b8f720fe62c4eee03c01f29076
azxca1731 <azxca1731@gmail.com>
2018-10-05 23:47:51 +0900

    express / 라우팅 수정 및 const선언

--- express/routes/index.js
+++ express/routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 module.exports = router;
