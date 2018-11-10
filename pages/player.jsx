45217a55e5747075aa6598b931c6ba762297485d
azxca1731 <azxca1731@gmail.com>
2018-11-11 01:48:18 +0900

    mock up 완료

--- pages/player.jsx
+++ pages/player.jsx
@@ -5,23 +5,36 @@ import 'video-react/dist/video-react.css'
 import { Player } from 'video-react'
 import VideoList from '../container/player/VideoList'
 import PlayerDiscription from '../components/player/PlayerDiscription'
-const Video = withRouter(props => (
-  <Layout>
-    <Container>
-      <Row className="mt-0 mt-md-4 mb-0 mb-md-4">
-        <Col xs="12" sm="12" md="8" lg="9" xl="9">
-          <Player
-            playsInline
-            src={`/api/?filename=${props.router.query.title}`}
-          />
-          <PlayerDiscription />
-        </Col>
-        <Col xs="12" sm="12" md="4" lg="3" xl="3">
-          <VideoList />
-        </Col>
-      </Row>
-    </Container>
-  </Layout>
-))
+const Video = withRouter(props => {
+  const mock = {
+    title: '신서유기 6 1화',
+    uploader: 'Junghun',
+    hashList: [{ title: '꿀잼보장' }, { title: '신서유기' }],
+    viewCount: 1
+  }
+  return (
+    <Layout>
+      <Container>
+        <Row className="mt-0 mt-md-4 mb-0 mb-md-4">
+          <Col xs="12" sm="12" md="8" lg="9" xl="9">
+            <Player
+              playsInline
+              src={`/api/?filename=${props.router.query.title}`}
+            />
+            <PlayerDiscription
+              title={mock.title}
+              uploader={mock.uploader}
+              hashList={mock.hashList}
+              viewCount={mock.viewCount}
+            />
+          </Col>
+          <Col xs="12" sm="12" md="4" lg="3" xl="3">
+            <VideoList />
+          </Col>
+        </Row>
+      </Container>
+    </Layout>
+  )
+})
 
 export default Video
