0682848e9ac8090b7b0293fffa26ad31ecb89e02
ponyong <ponyong@naver.com>
2018-12-02 18:17:16 +0900

    sidebar link fix , icon plus , text css fix

--- components/layout/SidebarContent.jsx
+++ components/layout/SidebarContent.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import { Navbar, NavbarBrand, Nav, NavItem } from 'reactstrap'
 import Link from 'next/link'
+import {
+  FaFileUpload,
+  FaVideo,
+  FaKey,
+  FaCreativeCommonsBy
+} from 'react-icons/fa'
 
 const SidebarContent = props => {
   return (
@@ -8,42 +14,53 @@ const SidebarContent = props => {
       <Navbar className="bg-light">
         <NavbarBrand>
           <Link href="/">
-            <span className="text-dark">NAStreamer</span>
+            <span className="sidebar_text">NAStreamer</span>
           </Link>
         </NavbarBrand>
       </Navbar>
       <Nav vertical>
-        <NavItem className="p-2">
-          <Link prefetch href="/video?category=all">
-            <a className="text-dark">To Videos</a>
+        <NavItem className="login p-2">
+          <Link href="/login">
+            <a className="sidebar_text">
+              <span>
+                <FaKey /> Login
+              </span>
+            </a>
           </Link>
         </NavItem>
-        <NavItem className="p-2">
-          <Link href="#">
-            <a className="text-dark">SideLink</a>
+        <NavItem className="sign_up p-2">
+          <Link prefetch href="/signup p-2">
+            <a className="sidebar_text">
+              <span>
+                <FaCreativeCommonsBy /> Sign Up
+              </span>
+            </a>
           </Link>
         </NavItem>
-        <NavItem className="p-2">
-          <Link href="#">
-            <a className="text-dark">SideLink</a>
+        <NavItem className="upload p-2">
+          <Link href="/uploader">
+            <a className="sidebar_text">
+              <span>
+                <FaFileUpload /> Upload
+              </span>
+            </a>
           </Link>
         </NavItem>
-        <NavItem className="p-2">
-          <Link href="#">
-            <a className="text-dark">SideLink</a>
-          </Link>
-        </NavItem>
-        <NavItem className="p-2">
-          <Link href="#">
-            <a className="text-dark">SideLink</a>
-          </Link>
-        </NavItem>
-        <NavItem className="p-2">
-          <Link href="#">
-            <a className="text-dark">SideLink</a>
+        <NavItem className="video p-2">
+          <Link href="/video">
+            <a className="sidebar_text">
+              <span>
+                <FaVideo /> Video
+              </span>
+            </a>
           </Link>
         </NavItem>
       </Nav>
+      <style jsx>{`
+        .sidebar_text {
+          color: #000000;
+        }
+      `}</style>
     </div>
   )
 }
