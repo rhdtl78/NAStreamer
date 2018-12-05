0814580387c527069fa2de4f83f2dd3569a791be
ponyong <ponyong@naver.com>
2018-12-05 23:16:13 +0900

    Sidebar feedback fix

--- components/layout/SidebarContent.jsx
+++ components/layout/SidebarContent.jsx
@@ -8,13 +8,31 @@ import {
   FaCreativeCommonsBy
 } from 'react-icons/fa'
 
+const SidebarSet = props => (
+  <div>
+    <a className="Sidebar__text">
+      <span className="Sidebar__icon">{props.icon}</span>
+      <span>{props.name}</span>
+    </a>
+    <style jsx>{`
+      .Sidebar__text {
+        color: #000000;
+        margin-left: 6px;
+      }
+      .Sidebar__icon {
+        margin-right: 8px;
+      }
+    `}</style>
+  </div>
+)
+
 const SidebarContent = props => {
   return (
     <div>
       <Navbar className="bg-light">
         <NavbarBrand>
           <Link href="/">
-            <div className="sidebar_maintext">
+            <div className="Sidebar__maintext">
               <span> NAStreamer</span>
             </div>
           </Link>
@@ -23,57 +41,32 @@ const SidebarContent = props => {
       <Nav vertical>
         <NavItem className="login p-3">
           <Link href="/login">
-            <a className="sidebar_text">
-              <span className="sidebar_icon">
-                <FaKey />
-              </span>
-              <span>Login</span>
-            </a>
+            <SidebarSet icon={<FaKey />} name="Login" />
           </Link>
         </NavItem>
         <NavItem className="sign_up p-3">
           <Link prefetch href="/signup">
-            <a className="sidebar_text">
-              <span className="sidebar_icon">
-                <FaCreativeCommonsBy />
-              </span>
-              <span>Sign Up</span>
-            </a>
+            <SidebarSet icon={<FaCreativeCommonsBy />} name="Sign Up" />
           </Link>
         </NavItem>
         <NavItem className="upload p-3">
           <Link href="/uploader">
-            <a className="sidebar_text">
-              <span className="sidebar_icon">
-                <FaFileUpload />
-              </span>
-              <span>Upload</span>
-            </a>
+            <SidebarSet icon={<FaFileUpload />} name="Upload" />
           </Link>
         </NavItem>
         <NavItem className="video p-3">
           <Link href="/video">
-            <a className="sidebar_text">
-              <span className="sidebar_icon">
-                <FaVideo />
-              </span>
-              <span>Video</span>
-            </a>
+            <SidebarSet icon={<FaVideo />} name="Video" />
           </Link>
         </NavItem>
       </Nav>
-      <style jsx>{`
-        .sidebar_text {
-          color: #000000;
-          margin-left: 6px;
-        }
-        .sidebar_maintext {
-          width: 230px;
-        }
-        .sidebar_icon {
-          margin-right: 8px;
-        }
-      `}</style>
+      <style jsx>
+        {`
+          .Sidebar__maintext {
+            width: 230px;
+          }
+        `}
+      </style>
     </div>
   )
 }
